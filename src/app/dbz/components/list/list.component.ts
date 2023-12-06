a6a9417a13fd7d66757dcf509a62c768852f20d9
Rodolfo <rodolfo.silvaa@idiem.cl>
2023-12-06 16:49:48 -0300

    dbz parte 1

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent { }
